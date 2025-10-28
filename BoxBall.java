da89860fea8ffa606cee07a2804d0c7d80ea385b
CGorosica2504 <145616433+CGorosica2504@users.noreply.github.com>
2025-10-27 23:12:44 -0400

    Updated the BoxBall class
    Updated the BoxBall class to add movement and wall collision.

--- BoxBall.java
+++ BoxBall.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.Random;
 
 /**
  * Class BoxBall - a graphical ball that moves similar to the ball in PONG.
@@ -9,8 +10,8 @@ import java.awt.geom.*;
  * Due to the way computer graphics are drawn using awt, the ball moves
  * DOWN when yspeed is positive and UP when yspeed is negative.
  * 
- * It is the ball's responsibility to determine if it has hit a wall and 
- * to reverse direction
+ * The ball will bounce when it collides with any wall of the box so that it remains inside.
+ * It will also avoid clipping into the boundaries of the box as much as possible.
  *
  * This movement can be initiated by repeated calls to the "move" method.
  * 
@@ -18,8 +19,9 @@ import java.awt.geom.*;
  * @author David J. Barnes
  * @author Bruce Quig
  * @author William Crosbie
+ * @author Christian Gorosica
  *
- * @version 2025.10.06
+ * @version 2025.10.27
  */
 
 public class BoxBall
@@ -34,9 +36,12 @@ public class BoxBall
     private Canvas canvas;
     private int ySpeed;         // vertical speed
     private int xSpeed;         // horizontal speed
+    private static final int MAX_SPEED = 7; //Maximum speed limit for the balls
+    private static final Random rand = new Random();
 
     /**
      * Constructor for objects of class BoxBall
+     * The ball is assigned a random x and y velocity within -7 and 7.
      *
      * @param xPos  the horizontal coordinate of the ball
      * @param yPos  the vertical coordinate of the ball
@@ -52,8 +57,24 @@ public class BoxBall
         yPosition = yPos;
         color = ballColor;
         diameter = ballDiameter;
+        myBox = box;
 
         canvas = drawingCanvas;
+        
+        //Setting random nonzero speeds between -7 and 7 for the balls' initial state
+        
+        //To generate nonzero xSpeed
+        xSpeed = 0;
+        while (xSpeed == 0) {
+            xSpeed = rand.nextInt(2 * MAX_SPEED + 1) - MAX_SPEED;
+
+        }
+        
+        //To generate nonzero ySpeed
+        ySpeed = 0;
+        while (ySpeed == 0) {
+            ySpeed = rand.nextInt(2 * MAX_SPEED + 1) - MAX_SPEED;
+        }
     }
 
     /**
@@ -75,6 +96,9 @@ public class BoxBall
 
     /**
      * Move this ball according to its position and speed and redraw.
+     * This method will check for collisions against the box walls. It will simulate bounces
+     * by reversing velocity values. As such, the ball will remain inside the box at all
+     * times.
      **/
     public void move()
     {
@@ -82,10 +106,36 @@ public class BoxBall
         erase();
             
         // compute new position
+        xPosition += xSpeed;
+        yPosition += ySpeed;
   
         // figure out if it has hit the left or right wall
         
+        //Left wall hit logic
+        if (xPosition <= myBox.getLeftWall()) {
+            xPosition = myBox.getLeftWall();
+            xSpeed = -xSpeed;
+        }
+        
+        //Right wall hit logic
+        if (xPosition + diameter >= myBox.getRightWall()) {
+            xPosition = myBox.getRightWall() - diameter;
+            xSpeed = -xSpeed;
+        }
+        
         // figure out if it has hit the top or bottom wall
+        
+        //Top wall hit logic
+        if (yPosition <= myBox.getTopWall()) {
+            yPosition = myBox.getTopWall();
+            ySpeed = -ySpeed;
+        }
+        
+        //Bottom wall hit logic
+        if (yPosition + diameter >= myBox.getBottomWall()) {
+            yPosition = myBox.getBottomWall() - diameter;
+            ySpeed = -ySpeed;
+        }
         
         draw();
     }    
